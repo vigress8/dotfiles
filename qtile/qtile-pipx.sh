9d7e74ee6de727fc3de8d4750ffad3adbc170812
vigress8 <150687949+vigress8@users.noreply.github.com>
2024-02-24 16:44:47 +0400

    update qtile-pipx.sh

--- qtile/qtile-pipx.sh
+++ qtile/qtile-pipx.sh
@@ -1,41 +1,41 @@
 #!/usr/bin/env bash
+set -euo pipefail
+
+WAYLAND_BACKEND=NO
+INSTALL_OPTDEPENDS=NO
+INSTALL_EXTRAS=NO
 
 depends=(
-  "libnotify4"
-  "libpangocairo-1.0-0"
-  "libpulse0"
-  "python3.11"
-  "xserver-xorg"
+  libnotify4
+  libpangocairo-1.0-0
+  libpulse0
+  python3.11
+  xserver-xorg
 )
 makedepends=(
-  "pkg-config"
-  "libiw-dev"
-  "libpulse-dev"
-  "libpython3.11-dev"
-  "pipx"
+  libiw-dev
+  libpulse-dev
+  libpython3.11-dev
+  pipx
+  pkg-config
 )
 optdepends=(
-  "alsa-utils" # volume widget
-  "cmus" # cmus widget
-  "jupyter-console" # for interaction with qtile via Jupyter
-  "khal" # for khal_calendar widget
-  "lm-sensors" # for sensors widget
-  "moc" # for moc widget
+  alsa-utils # volume widget
+  cmus # cmus widget
+  jupyter-console # for interaction with qtile via Jupyter
+  khal # for khal_calendar widget
+  lm-sensors # for sensors widget
+  moc # for moc widget
 )
-
-
-WAYLAND_BACKEND="NO"
-INSTALL_OPTDEPENDS="NO"
-INSTALL_EXTRAS="NO"
 
 read -p "Install Wayland backend? (y/N)" -r
 case "$REPLY" in
   y* | Y*)
     if apt-cache show libwlroots11 | grep -q 'Version: 0.16'; then
-      WAYLAND_BACKEND="YES"
-      depends+=("libwlroots11")
-      makedepends+=("libwlroots-dev")
-      optdepends+=("xwayland")
+      WAYLAND_BACKEND=YES
+      depends+=(libwlroots11)
+      makedepends+=(libwlroots-dev)
+      optdepends+=(xwayland)
     else
       echo "Sorry, looks like wlroots 0.16 is not available in your system's repositories."
     fi
@@ -43,77 +43,75 @@ case "$REPLY" in
   *) ;;
 esac
 
-cat << END
-Runtime dependencies: ${depends[@]}
-Build dependencies: ${makedepends[@]}
-Optional dependencies: ${optdepends[@]}
-END
+echo "
+Runtime dependencies: ${depends[*]}
+Build dependencies: ${makedepends[*]}
+Optional dependencies: ${optdepends[*]}
+"
 read -p "Install optional dependencies? (y/N)" -r
 case "$REPLY" in
-  y* | Y*) INSTALL_OPTDEPENDS="YES" ;;
+  y* | Y*) INSTALL_OPTDEPENDS=YES ;;
   *) ;;
 esac
 
 read -p "Install qtile-extras? (y/N)" -r
 case "$REPLY" in
-  y* | Y*) INSTALL_EXTRAS="YES" ;;
+  y* | Y*) INSTALL_EXTRAS=YES ;;
   *) ;;
 esac
 
-cat << END
+echo "
 This is what shall be installed with apt:
-Runtime dependencies: ${depends[@]}
-Build dependencies: ${makedepends[@]}
-END
-[[ $INSTALL_OPTDEPENDS = "YES" ]] && cat << END
-Optional dependencies: ${optdepends[@]}
-END
-cat << END
-===================
+Runtime dependencies: ${depends[*]}
+Build dependencies: ${makedepends[*]}"
+[[ $INSTALL_OPTDEPENDS = YES ]] && \
+echo "Optional dependencies: ${optdepends[*]}"
+echo "
 Your chosen options:
 Install Wayland backend: $WAYLAND_BACKEND
 Install optional dependencies: $INSTALL_OPTDEPENDS
 Install qtile-extras: $INSTALL_EXTRAS
-END
+"
 
 read -p "Proceed? (y/N)" -r
 case "$REPLY" in
-  y|Y) ;;
+  y* | Y*) ;;
   *) exit 1 ;;
 esac
 
-echo "Starting installation..."
+echo Starting installation...
 mkdir -p /tmp/qtile-install
-pushd /tmp/qtile-install > /dev/null || exit 1
+pushd /tmp/qtile-install > /dev/null
 
-echo "Installing dependencies..."
+echo Installing dependencies...
 sudo apt install -y "${depends[@]}" "${makedepends[@]}"
-[[ $INSTALL_OPTDEPENDS = "YES" ]] && {
-  echo "Installing optional dependencies..."
+[[ $INSTALL_OPTDEPENDS == YES ]] && {
+  echo Installing optional dependencies...
   sudo apt install -y "${optdepends[@]}"
 }
 
-PIPX_VARS=(
-  "PIPX_HOME=/opt"
-  "PIPX_BIN_DIR=/usr/bin"
-  "PIPX_MAN_DIR=/usr/share/man"
-)
-echo "Installing qtile..."
-sudo "${PIPX_VARS[@]}" pipx install --python python3.11 --system-site-packages 'qtile[all] @ https://github.com/qtile/qtile.git'
-[[ $INSTALL_EXTRAS = "YES" ]] && {
-  echo "Installing qtile-extras..."
-  sudo "${PIPX_VARS[@]}" pipx inject qtile 'qtile-extras @ https://github.com/elparaguayo/qtile-extras.git'
-}
-curl -O "https://raw.githubusercontent.com/qtile/qtile/master/libqtile/resources/default_config.py"
-echo "Installing desktop files..."
-curl -O "https://raw.githubusercontent.com/qtile/qtile/master/resources/qtile.desktop"
-sudo install -Dm644 default_config.py -t /usr/share/doc/qtile
-sudo install -Dm644 qtile.desktop -t /usr/share/xsessions
-[[ $WAYLAND_BACKEND = "YES" ]] && {
-  curl -O "https://raw.githubusercontent.com/qtile/qtile/master/resources/qtile-wayland.desktop"
-  sudo install -Dm644 qtile-wayland.desktop -t /usr/share/wayland-sessions
-}
-sudo ln -s /opt/venvs/qtile/bin/qtile /usr/bin/qtile
+export PIPX_HOME=/opt
+export PIPX_BIN_DIR=/usr/bin
+export PIPX_MAN_DIR=/usr/share/man
 
-echo "Success."
-popd > /dev/null || exit 1
+echo Installing qtile...
+sudo -E pipx install --python python3.11 --system-site-packages 'qtile[all] @ https://github.com/qtile/qtile.git'
+
+[[ $INSTALL_EXTRAS == YES ]] && {
+  echo Installing qtile-extras...
+  sudo -E pipx inject qtile 'qtile-extras @ https://github.com/elparaguayo/qtile-extras.git'
+}
+
+echo Installing desktop files...
+sudo install -Dvm644 /dev/stdin /usr/share/doc/qtile/default_config.py \
+  <(curl -fsS "https://raw.githubusercontent.com/qtile/qtile/master/libqtile/resources/default_config.py")
+sudo install -Dvm644 /dev/stdin /usr/share/xsessions/qtile.desktop \
+<(curl -fsS "https://raw.githubusercontent.com/qtile/qtile/master/resources/qtile.desktop")
+
+[[ $WAYLAND_BACKEND == YES ]] && \
+  sudo install -Dvm644 /usr/share/wayland-sessions/qtile-wayland.desktop  \
+    <(curl -fsS "https://raw.githubusercontent.com/qtile/qtile/master/resources/qtile-wayland.desktop")
+
+sudo ln -s /opt/venvs/qtile/bin/qtile /usr/bin/qtile
+echo Success.
+popd > /dev/null
