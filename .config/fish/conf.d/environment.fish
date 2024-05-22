3adf53ef6f83191174b981a95fb147de94739bd6
vigress8 <150687949+vigress8@users.noreply.github.com>
2024-05-22 11:01:02 +0400

    .

--- .config/fish/conf.d/environment.fish
+++ .config/fish/conf.d/environment.fish
@@ -57,6 +57,9 @@ if command_present eza
   alias lla     'eza --icons -la'
   alias ls      'eza --icons'
 end
+alias router-ip 'route -n | awk \'/^0\.0\.0\.0/ {print $2}\''
 command_present yarnpkg; and alias yarn yarnpkg
+command_present fdfind; and alias fd fdfind
+command_present batcat; and alias bat batcat
 
 abbr --add discordo "discordo -token \$DISCORD_AUTH_TOKEN"
